958d5d4a3cf696049534e6495437afea394090dc
Jared <wilbrahamjared@gmail.com>
2022-05-23 16:28:14 -0400

    README Update

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
         <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
